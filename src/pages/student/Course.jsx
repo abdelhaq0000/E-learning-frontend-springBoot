c25830a474282520e58546fa455abae4f295cb34
abdelhaq elmetlini <93001592+abdelhaq0000@users.noreply.github.com>
2023-12-17 17:10:10 -0800

    Qualityadd

---
+++ src/pages/student/Course.jsx
@@ -0,0 +1,35 @@
+import CourseSection from '../../components/CourseSection'
+import {  Footer } from '../../components'
+import Pagination from '../../components/Pagination'
+import Reaction from '../../components/Reaction'
+import NavbarConextion from '../../components/NavbarConextion'
+import { useEffect } from 'react'
+import { useNavigate } from 'react-router-dom'
+
+const Course = () => {
+  const navigate = useNavigate();
+  useEffect(()=>{
+    if (!(localStorage.getItem("role") === "ROLE_STUDENT")) {
+      navigate("/")
+    }
+  })
+  return (
+    <main className='relative'>
+      <NavbarConextion />
+      <section className='padding-x pt-40'>
+        <CourseSection />
+      </section>
+      {/* <section className='padding-x pt-24'>
+        <Reaction />
+      </section>
+      <section className='padding-x pt-14'>
+        <Pagination length={5} />
+      </section>
+      <section className='bg-black padding-x padding-t pb-8 mt-12'>
+        <Footer />
+      </section> */}
+    </main>
+  )
+}
+
+export default Course
